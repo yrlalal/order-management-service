590e16bea4367ce2431957dca2e14632948999a0
Yash <yrlalal@gmail.com>
2024-02-26 16:10:05 -0800

    Initial commit

---
+++ src/test/java/com/yrlalal/ordermanagementservice/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrlalal.ordermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
