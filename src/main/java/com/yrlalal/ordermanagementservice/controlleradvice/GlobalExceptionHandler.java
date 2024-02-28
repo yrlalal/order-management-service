b925bbb4935a8bc82a80d6a3d940d3a7de0c1755
Yash <yrlalal@gmail.com>
2024-02-27 22:29:46 -0800

    Log exceptions

--- src/main/java/com/yrlalal/ordermanagementservice/controlleradvice/GlobalExceptionHandler.java
+++ src/main/java/com/yrlalal/ordermanagementservice/controlleradvice/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.yrlalal.ordermanagementservice.controlleradvice;
 
 import com.yrlalal.ordermanagementservice.exception.BadRequestException;
 import com.yrlalal.ordermanagementservice.exception.OrderNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,13 +11,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleException(BadRequestException ex) {
+        logger.error(ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
     public ResponseEntity<String> handleException(OrderNotFoundException ex) {
+        logger.error(ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        logger.error(ex.getMessage());
+        return new ResponseEntity<>("An internal error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
